fa1b7ae41a8a4016968a98326bc9625c724725b8
maryaalshameri <marynaalshameri@gmail.com>
2025-08-13 18:59:11 +0300

    Add server time display in index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// time server
+$serverTime = date('Y-m-d H:i:s');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Server Time & Form</title>
+</head>
+<body>
+    <h1>Current Server Time: <?= htmlspecialchars($serverTime) ?></h1>
+
+   
+</body>
+</html>
